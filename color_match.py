010fe06fa443c01164e328fbc699e2c0ef8705a3
oneilryle <143393747+ONeil-Liaga@users.noreply.github.com>
2024-11-04 22:49:25 +0300

    Implement FallingObject class with random color

--- color_match.py
+++ color_match.py
@@ -32,3 +32,29 @@ class Sphere:
     def change_color(self):
         self.color = random.choice(COLORS)
 
+# Define the FallingObject class with bouncing effect
+class FallingObject:
+    def _init_(self):
+        self.color = random.choice(COLORS)
+        self.x = random.randint(0, WIDTH - 20)
+        self.y = 0
+        self.x_velocity = random.choice([-3, 3])  # Random horizontal speed
+        self.y_velocity = FALL_SPEED              # Falling speed
+
+    def move(self):
+        # Update position
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+        # Bounce off the walls
+        if self.x <= 0 or self.x >= WIDTH - 20:
+            self.x_velocity *= -1  # Reverse x direction
+
+        # Bounce off the top and bottom
+        if self.y <= 0 or self.y >= HEIGHT - 20:
+            self.y_velocity *= -1  # Reverse y direction
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, 20, 20))
+
+
